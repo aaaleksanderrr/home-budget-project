ab2af8ec6a514b00e53f549f6d19c9c311ad657a
Aleksander Wypych <aleksander.wypych@gmail.com>
2024-07-12 23:39:28 +0200

    Created helper functions,  added wrapper divs and implemented initial logic for editing items

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const listExpenses = document.getElementById("listExpenses");
 let displaySumValueIncomes = 0;
 let displaySumValueExpenses = 0;
 
+const applyClass = (array, className) => {
+  array.forEach((element) => {
+    element.className = className;
+  });
+};
+
+const removeTags = (array) => {
+  array.forEach((element) => {
+    element.remove();
+  });
+};
+
 sumValueIncomes.innerText = displaySumValueIncomes;
 sumValueExpenses.innerText = displaySumValueExpenses;
 
@@ -20,18 +32,23 @@ const handleClick = (type) => {
   const li = document.createElement("li");
   type === "Income" ? listIncomes.appendChild(li) : listExpenses.appendChild(li);
 
+  const divLi = document.createElement("div");
+  divLi.className = "li-wrapper";
+
+  const divItem = document.createElement("div");
+  divItem.className = "item-wrapper";
+
   const spanName = document.createElement("span");
   type === "Income"
-    ? (spanName.innerText = nameIncome.value + " - ")
-    : (spanName.innerText = nameExpense.value + " - ");
-  li.appendChild(spanName);
+    ? (spanName.innerText = nameIncome.value)
+    : (spanName.innerText = nameExpense.value);
+  spanName.className = "display-separator";
 
   const spanValue = document.createElement("span");
   type === "Income"
     ? (spanValue.innerText = valueIncome.value)
     : (spanValue.innerText = valueExpense.value);
   spanValue.className = "display-currency text-bold";
-  li.appendChild(spanValue);
 
   type === "Income"
     ? (displaySumValueIncomes += Number(valueIncome.value))
@@ -40,6 +57,62 @@ const handleClick = (type) => {
   type === "Income"
     ? (sumValueIncomes.innerText = displaySumValueIncomes)
     : (sumValueExpenses.innerText = displaySumValueExpenses);
+
+  const divButtons = document.createElement("div");
+  divButtons.className = "button-wrapper";
+
+  const btnEdit = document.createElement("button");
+  btnEdit.innerText = "Edytuj";
+  const btnRemove = document.createElement("button");
+  btnRemove.innerText = "Usuń";
+
+  li.appendChild(divLi);
+  divItem.append(spanName, spanValue);
+  divButtons.append(btnEdit, btnRemove);
+  divLi.append(divItem, divButtons);
+
+  const handleItemClick = (type) => {
+    if (type === "Edit") {
+      applyClass([spanName, spanValue, btnEdit, btnRemove], "hidden");
+
+      const editNameForm = document.createElement("input");
+      editNameForm.type = "text";
+      editNameForm.className = "form-element-name";
+      editNameForm.value = spanName.innerText;
+
+      const editValueForm = document.createElement("input");
+      editValueForm.type = "text";
+      editValueForm.className = "form-element-value";
+      editValueForm.value = spanValue.innerText;
+
+      const saveBtn = document.createElement("button");
+      saveBtn.innerText = "Zapisz";
+
+      const cancelBtn = document.createElement("button");
+      cancelBtn.innerText = "Anuluj";
+
+      divItem.append(editNameForm, editValueForm, saveBtn, cancelBtn);
+
+      const handleEditClick = (type) => {
+        if (type === "Cancel") {
+          removeTags([editNameForm, editValueForm, saveBtn, cancelBtn]);
+
+          btnEdit.classList.remove("hidden");
+          btnRemove.classList.remove("hidden");
+          spanName.className = "display-separator";
+          spanValue.className = "display-currency text-bold";
+        } else if (type === "Save") {
+          // to do
+        }
+      };
+
+      cancelBtn.addEventListener("click", () => handleEditClick("Cancel"));
+      saveBtn.addEventListener("click", () => handleEditClick("Save"));
+    }
+  };
+
+  btnEdit.addEventListener("click", () => handleItemClick("Edit"));
+  btnRemove.addEventListener("click", () => handleItemClick("Remove"));
 };
 
 btnAddIncome.addEventListener("click", () => handleClick("Income"));
